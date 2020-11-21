cfa74e8ccfc5692cc923abb4d3a9693997eeb4e3
Janik Rabe <info@janikrabe.com>
2020-11-21 14:30:12 +0000

    [all] Update email address and/or project URL

--- src/l1sign_cmd_pubkey.h
+++ src/l1sign_cmd_pubkey.h
@@ -1,6 +1,6 @@
 /*
  * l1sign - Implementation of the Lamport one-time signature scheme
- * Copyright (c) 2019  Janik Rabe <l1sign@janikrabe.com>
+ * Copyright (c) 2019  Janik Rabe <info@janikrabe.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
